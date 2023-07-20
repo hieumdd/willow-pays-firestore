79c1608ecf078a3292609e1f5130253ac974bc9a
HM <hieumdd@gmail.com>
2023-07-20 08:13:03 +0700

    feat: Add subcollection

--- src/pipeline/pipeline.const.ts
+++ src/pipeline/pipeline.const.ts
@@ -17,7 +17,7 @@ export const Events: Pipeline = {
     get: () => {
         const events = firestore.collection('events').stream();
 
-        const payments = new Transform({
+        const subcollections = new Transform({
             objectMode: true,
             transform: (row: DocumentSnapshot, _, callback) => {
                 row.ref
@@ -72,7 +72,7 @@ export const Events: Pipeline = {
             },
         });
 
-        return events.pipe(payments).pipe(transform);
+        return events.pipe(subcollections).pipe(transform);
     },
     table: 'Events',
     schema: [
@@ -113,12 +113,52 @@ export const Events: Pipeline = {
 
 export const CustomerAccounts: Pipeline = {
     get: () => {
+        let count = 0;
+
         const stream = firestore.collection('customerAccounts').stream();
 
-        const parse = new Transform({
+        const subcollections = new Transform({
             objectMode: true,
-            transform: (row: DocumentSnapshot, _, callback) => {
-                callback(null, { id: row.id, data: row.data() });
+            transform: async (row: DocumentSnapshot, _, callback) => {
+                const collections = [
+                    'bankTransactionSummaries',
+                    'customerBills',
+                    'events',
+                ] as const;
+
+                Promise.all(
+                    collections.map((collection) => {
+                        return row.ref
+                            .collection(collection)
+                            .listDocuments()
+                            .then((refs) => Promise.all(refs.map((ref) => ref.get())));
+                    }),
+                )
+                    .then((snapshots) => {
+                        const [bankTransactionSummaries, customerBills, events] = snapshots;
+
+                        callback(null, {
+                            id: row.id,
+                            data: {
+                                ...row.data(),
+                                bankTransactionSummaries: bankTransactionSummaries.map(
+                                    (snapshot) => ({
+                                        id: snapshot.id,
+                                        data: snapshot.data(),
+                                    }),
+                                ),
+                                customerBills: customerBills.map((snapshot) => ({
+                                    id: snapshot.id,
+                                    data: snapshot.data(),
+                                })),
+                                events: events.map((snapshot) => ({
+                                    id: snapshot.id,
+                                    data: snapshot.data(),
+                                })),
+                            },
+                        });
+                    })
+                    .catch((error) => callback(error));
             },
         });
 
@@ -138,18 +178,65 @@ export const CustomerAccounts: Pipeline = {
                 stripeId: Joi.string(),
                 verifiedMobilePhoneNumber: Joi.string(),
                 willowCreditLimit: Joi.number().unsafe(),
+                bankTransactionSummaries: Joi.array().items({
+                    id: Joi.string(),
+                    data: Joi.object({
+                        balanceDelta: Joi.number().unsafe(),
+                        code: Joi.string(),
+                        customerAccountId: Joi.string(),
+                        depositAmountTotal: Joi.number().unsafe(),
+                        depositCount: Joi.number().unsafe(),
+                        endDate: timestamp,
+                        overdraft: Joi.boolean(),
+                        salariesAmountTotal: Joi.number().unsafe(),
+                        startDate: timestamp,
+                        transactionCount: Joi.number().unsafe(),
+                        withdrawalAmountTotal: Joi.number().unsafe(),
+                        withdrawalCount: Joi.number().unsafe(),
+                    }),
+                }),
+                customerBills: Joi.array().items({
+                    id: Joi.string(),
+                    data: Joi.object({
+                        billAmount: Joi.number().unsafe(),
+                        billDueDate: timestamp,
+                        billPayToName: Joi.string(),
+                        billRepaymentMethod: Joi.string(),
+                        billRepaymentsSelection: Joi.number().unsafe(),
+                        billStatus: Joi.string(),
+                        billType: Joi.string(),
+                        created: timestamp,
+                        customerAccountId: Joi.string(),
+                        displayId: Joi.string(),
+                        displayName: Joi.string(),
+                        id: Joi.string(),
+                        modified: timestamp,
+                        numberOfPayments: Joi.number().unsafe(),
+                    }),
+                }),
+                events: Joi.array().items({
+                    id: Joi.string(),
+                    data: Joi.object({
+                        customerAccountId: Joi.string(),
+                        emailResult: Joi.string(),
+                        timestamp: timestamp,
+                        trigger: Joi.string(),
+                    }),
+                }),
             }),
         });
 
         const transform = new Transform({
             objectMode: true,
             transform: (row: any, _, callback) => {
+                count = count + 1;
+                console.log(count);
                 const { value, error } = schema.validate(row, { stripUnknown: true });
                 value ? callback(null, value) : callback(error);
             },
         });
 
-        return stream.pipe(parse).pipe(transform);
+        return stream.pipe(subcollections).pipe(transform);
     },
     table: 'CustomerAccounts',
     schema: [
@@ -171,6 +258,78 @@ export const CustomerAccounts: Pipeline = {
                 { name: 'stripeId', type: 'STRING' },
                 { name: 'verifiedMobilePhoneNumber', type: 'STRING' },
                 { name: 'willowCreditLimit', type: 'NUMERIC' },
+                {
+                    name: 'bankTransactionSummaries',
+                    type: 'RECORD',
+                    mode: 'REPEATED',
+                    fields: [
+                        { name: 'id', type: 'STRING' },
+                        {
+                            name: 'data',
+                            type: 'RECORD',
+                            fields: [
+                                { name: 'balanceDelta', type: 'NUMERIC' },
+                                { name: 'code', type: 'STRING' },
+                                { name: 'customerAccountId', type: 'STRING' },
+                                { name: 'depositAmountTotal', type: 'NUMERIC' },
+                                { name: 'depositCount', type: 'NUMERIC' },
+                                { name: 'endDate', type: 'TIMESTAMP' },
+                                { name: 'overdraft', type: 'BOOLEAN' },
+                                { name: 'salariesAmountTotal', type: 'NUMERIC' },
+                                { name: 'startDate', type: 'TIMESTAMP' },
+                                { name: 'transactionCount', type: 'NUMERIC' },
+                                { name: 'withdrawalAmountTotal', type: 'NUMERIC' },
+                                { name: 'withdrawalCount', type: 'NUMERIC' },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    name: 'customerBills',
+                    type: 'RECORD',
+                    mode: 'REPEATED',
+                    fields: [
+                        { name: 'id', type: 'STRING' },
+                        {
+                            name: 'data',
+                            type: 'RECORD',
+                            fields: [
+                                { name: 'billAmount', type: 'NUMERIC' },
+                                { name: 'billDueDate', type: 'TIMESTAMP' },
+                                { name: 'billPayToName', type: 'STRING' },
+                                { name: 'billRepaymentMethod', type: 'STRING' },
+                                { name: 'billRepaymentsSelection', type: 'STRING' },
+                                { name: 'billStatus', type: 'STRING' },
+                                { name: 'billType', type: 'STRING' },
+                                { name: 'created', type: 'TIMESTAMP' },
+                                { name: 'customerAccountId', type: 'STRING' },
+                                { name: 'displayId', type: 'STRING' },
+                                { name: 'displayName', type: 'STRING' },
+                                { name: 'id', type: 'STRING' },
+                                { name: 'modified', type: 'TIMESTAMP' },
+                                { name: 'numberOfPayments', type: 'NUMERIC' },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    name: 'events',
+                    type: 'RECORD',
+                    mode: 'REPEATED',
+                    fields: [
+                        { name: 'id', type: 'STRING' },
+                        {
+                            name: 'data',
+                            type: 'RECORD',
+                            fields: [
+                                { name: 'customerAccountId', type: 'STRING' },
+                                { name: 'emailResult', type: 'STRING' },
+                                { name: 'timestamp', type: 'TIMESTAMP' },
+                                { name: 'trigger', type: 'STRING' },
+                            ],
+                        },
+                    ],
+                },
             ],
         },
     ],
