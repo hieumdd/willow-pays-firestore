33cc374ffb12598f46684337a3dd8d40bea01abb
HM <hieumdd@gmail.com>
2023-07-25 20:55:55 +0700

    feat: Add SIGINT

--- src/single.ts
+++ src/single.ts
@@ -1,6 +1,8 @@
+import process from 'process';
 require('dotenv').config();
 import { program } from 'commander';
 
+import { logger } from './logging.service';
 import * as pipelines from './pipeline/pipeline.const';
 import { runPipeline } from './pipeline/pipeline.service';
 
@@ -16,3 +18,8 @@ program.parse(process.argv);
 
     await runPipeline(pipelines[pipeline]);
 })();
+
+process.on('SIGINT', () => {
+    logger.info({ action: 'interupt' });
+    process.exit(0);
+});
